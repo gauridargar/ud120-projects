3d193ec7a85877a19cf96ff5e55c5a7cf9fcaf54
gauri dargar <74265612+gauridargar@users.noreply.github.com>
2020-12-12 17:30:29 +0530

    Update outlier_cleaner.py

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,8 +10,13 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
+    import operator
     
-    cleaned_data = []
+    errors = [a-b for a,b in zip(predictions,net_worths)]
+    ata = zip(ages, net_worths, errors)
+    data.sort(key=operator.itemgetter(2))
+    cleaned_data = data[:int(len(predictions)*0.9)]
+    
 
     ### your code goes here
 
